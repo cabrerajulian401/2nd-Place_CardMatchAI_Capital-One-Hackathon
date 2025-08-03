8f05aa77b7cbe61d623e18838630262757d565d4
Julian Cabrera <cabrerajulian401@gmail.com>
2025-08-03 01:48:16 -0500

    Changing URL to Render Backend  For Hosing

--- Frontend/src/api/agent.js
+++ Frontend/src/api/agent.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000"; // Updated to use port 3001
+const BASE_URL = "https://twond-place-creditmatchai-capital-one.onrender.com"; // Updated to use port 3001
 
 export async function startConversation() {
   try {
